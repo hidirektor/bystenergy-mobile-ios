3a0c092964763a6e6415660e6e6ea33b61241b5e
hidirektor <hidirektor@gmail.com>
2025-02-26 01:53:02 +0300

    Initial Commit

---
+++ bystenergy/bystenergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bystenergyApp.swift
+//  bystenergy
+//
+//  Created by Halil İbrahim Direktör on 26.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct bystenergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bystenergyTests/bystenergyTests.swift
@@ -0,0 +1,17 @@
+//
+//  bystenergyTests.swift
+//  bystenergyTests
+//
+//  Created by Halil İbrahim Direktör on 26.02.2025.
+//
+
+import Testing
+@testable import bystenergy
+
+struct bystenergyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
